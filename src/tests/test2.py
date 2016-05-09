e6537e005e079586f62c41d8fcb25ea65c0025cd
Simon Smart <simon.smart@ecmwf.int>
2016-05-09 17:21:23 +0100

    Tidy test2.py

--- src/tests/test2.py
+++ src/tests/test2.py
@@ -1,70 +1,61 @@
+import os
 
-#////////////////////////////////////////////////////////////////
+# Add parent directory as search path form modules
+os.sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from config.config import Config
+from RealWorkload import RealWorkload
+from IOWSModel import IOWSModel
+from WorkloadCorrector import WorkloadCorrector
+from PlotHandler import PlotHandler
+
+import numpy as np
+
+
 def test2():
 
-    #================================================================
-    ConfigOptions = Config()
-#    plot_tag = "test5_FFT_Kmeans_SELCLUST"
-#    plot_tag = "test5_FFT_SOM"
+    # Load config
+
+    config = Config()
     plot_tag = "test_NEW_"
 
-    #================================================================
-    InputWorkload = RealWorkload(ConfigOptions)
-    InputWorkload.read_PBS_logs("/perm/ma/maab/PBS_log_example/20151123_test_2k")
-#    InputWorkload.read_PBS_logs("/perm/ma/maab/PBS_log_example/20151123_test100")
-    InputWorkload.calculate_derived_quantities()
-    InputWorkload.make_plots(plot_tag)
+    # Initialise the input workload
+    input_workload = RealWorkload(config)
+    input_workload.read_PBS_logs("/perm/ma/maab/PBS_log_example/20151123_test_2k")
+#    input_workload.read_PBS_logs("/perm/ma/maab/PBS_log_example/20151123_test100")
+    input_workload.calculate_derived_quantities()
+    input_workload.make_plots(plot_tag)
 
-    #================================================================
-    Corrector = WorkloadCorrector(InputWorkload, ConfigOptions)
-    Corrector.replace_missing_data("ANN")
-    #Corrector.enrich_data_with_TS("FFT")
-    Corrector.enrich_data_with_TS("bins")
-    Corrector.calculate_global_metrics()
-    Corrector.plot_missing_data(plot_tag)
-    Corrector.make_plots(plot_tag)
+    # Adjust input workload for missing fields
+    # n.b. input_data is modified internal within corrector
 
-    #================================================================
-    Model = IOWSModel(ConfigOptions)
-    Model.set_input_workload(InputWorkload)
-    
-    #------------ cases with spacified number of clusters --------------
-    for iNC in np.append(np.arange(1, len(InputWorkload.LogData) + 1, 
-                                   int(len(InputWorkload.LogData) / 5)), 
-                                   len(InputWorkload.LogData)):
+    corrector = WorkloadCorrector(input_workload, config)
+    corrector.replace_missing_data("ANN")
+    # Corrector.enrich_data_with_TS("FFT")
+    corrector.enrich_data_with_TS("bins")
+    corrector.calculate_global_metrics()
+    corrector.plot_missing_data(plot_tag)
+    corrector.make_plots(plot_tag)
+
+    # Generator model
+
+    model = IOWSModel(config)
+    model.set_input_workload(input_workload)
+
+    # Consider clustering with differing number of clusters
+
+    for num_clusters in np.append(np.arange(1, len(input_workload.LogData) + 1,
+                                   int(len(input_workload.LogData) / 5)),
+                                   len(input_workload.LogData)):
                                        
-        Model.apply_clustering("time_plane", "Kmeans", iNC)
-        #Model.apply_clustering("spectral", "Kmeans", iNC)
-        #Model.apply_clustering("spectral", "DBSCAN")
-        Model.make_plots( plot_tag )
+        model.apply_clustering("time_plane", "Kmeans", num_clusters)
+        # model.apply_clustering("spectral", "Kmeans", num_clusters)
+        # model.apply_clustering("spectral", "DBSCAN")
+        model.make_plots(plot_tag)
 
-
-    #================================================================
     PlotHandler.print_fig_handle_ID()
-    
-#////////////////////////////////////////////////////////////////
 
 
-#////////////////////////////////////////////////////////////////
-if __name__ == '__main__' and __package__ is None:
-    from os import sys, path
-    import numpy as np
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-    
-    from config.config import Config
-    from RealWorkload import RealWorkload
-    from IOWSModel import IOWSModel
-    from WorkloadCorrector import WorkloadCorrector
-    from PlotHandler import PlotHandler
+if __name__ == '__main__':
 
     test2()
-#////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
