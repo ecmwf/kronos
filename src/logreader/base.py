2472e2e61854a8fa5a95cf2abc44d7948c74ece8
Simon Smart <simon.smart@ecmwf.int>
2016-07-22 17:53:29 +0100

    File iteration and selection in the LogReader base class

--- src/logreader/base.py
+++ src/logreader/base.py
@@ -1,3 +1,6 @@
+import fnmatch
+import os
+
 from jobs import IngestedJob
 
 
@@ -19,14 +22,41 @@ class LogReader(object):
         if file_pattern:
             self.file_pattern = file_pattern
 
+    def __unicode__(self):
+        return "LogReader({})[{}]".format(self.__class__.__name__, self.path)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
     def logfiles(self):
         """
         If the specified path is an (existing) file, then just return that file. If it is a directory, then
         iterate through the matching
         :return:
         """
-        raise NotImplementedError
-        yield ""
+        if not os.path.exists(self.path):
+            raise IOError("Path {} does not exist (specified for {})".format(self.path, self))
+
+        if os.path.isfile(self.path):
+            # If the specified path is a file, then it should be directly returned
+            yield self.path
+
+        elif self.recursive:
+            # Find files in the specified path RECURSIVELY, matching the file pattern if relevant
+            for root, dirs, files in os.walk(self.path):
+                pattern = os.path.join(self.path, self.file_pattern) if self.file_pattern else None
+                for filename in files:
+                    full_filename = os.path.join(root, filename)
+                    if pattern is None or fnmatch.fnmatch(full_filename, pattern):
+                        yield full_filename
+
+        else:
+            # Find files in the specified path (non-recursively), matching the file pattern if relevant
+            pattern = os.path.join(self.path, self.file_pattern) if self.file_pattern else None
+            for filename in os.listdir(self.path):
+                fn = os.path.join(self.path, filename)
+                if os.path.isfile(fn) and (pattern is None or fnmatch.fnmatch(fn, pattern)):
+                    yield fn
 
     def read_log(self, filename):
         raise NotImplementedError
--- src/tests/logreader/test_base_logreader.py
+++ src/tests/logreader/test_base_logreader.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
+import types
 import unittest
+import tempfile
+import shutil
 import os
 
 from logreader.base import LogReader
@@ -35,6 +38,186 @@ class BaseLogReaderTest(unittest.TestCase):
 
         lr = LogReaderSubclass('fake-path')
         self.assertEqual(lr.file_pattern, "default_pattern")
+
+    def test_logfiles_nonexistent(self):
+        """
+        If the specified path does not exist, then an exception should be raised when trying to iterate
+        through the available files.
+        """
+        self.assertRaises(IOError, LogReader('fake-path').logfiles().next)
+
+    def test_logfiles_single_file(self):
+        """
+        If the path provided is a single file, then it should return that.
+        """
+        with tempfile.NamedTemporaryFile() as f:
+
+            lr = LogReader(f.name)
+            assert isinstance(lr.logfiles(), types.GeneratorType)
+            files = [x for x in lr.logfiles()]
+
+            self.assertEqual(files, [f.name])
+
+    def test_logfiles_directory(self):
+        """
+        We should enumerate the matching files from the specified path, without recursively
+        descending into subdirectories
+        """
+        dir_list = ['dir1', 'dir2/dir3']
+
+        file_list = [
+            'file1.log',
+            'file2.log',
+            'dir1/file3.log',
+            'dir2/dir3/file4.log',
+            'other1',
+            'other2',
+            'dir1/other3'
+        ]
+
+        target_files = [
+            'file1.log',
+            'file2.log',
+            'other1',
+            'other2'
+        ]
+
+        # Create a temporary directory. try/finally ensures that it _WILL_ be deleted
+        directory = tempfile.mkdtemp()
+        try:
+            for d in dir_list:
+                os.makedirs(os.path.join(directory, d))
+            for fn in file_list:
+                # Create an (empty) file
+                open(os.path.join(directory, fn), 'a').close()
+
+            lr = LogReader(directory)
+            assert isinstance(lr.logfiles(), types.GeneratorType)
+            files = sorted([x for x in lr.logfiles()])
+
+            self.assertEqual(files, sorted([os.path.join(directory, fn) for fn in target_files]))
+
+        finally:
+            shutil.rmtree(directory)
+
+    def test_logfiles_directory_pattern(self):
+        """
+        We should enumerate the matching files from the specified path, without recursively
+        descending into subdirectories
+        """
+        dir_list = ['dir1', 'dir2/dir3']
+
+        file_list = [
+            'file1.log',
+            'file2.log',
+            'dir1/file3.log',
+            'dir2/dir3/file4.log',
+            'other1',
+            'other2',
+            'dir1/other3'
+        ]
+
+        target_files = [
+            'file1.log',
+            'file2.log',
+        ]
+
+        # Create a temporary directory. try/finally ensures that it _WILL_ be deleted
+        directory = tempfile.mkdtemp()
+        try:
+            for d in dir_list:
+                os.makedirs(os.path.join(directory, d))
+            for fn in file_list:
+                # Create an (empty) file
+                open(os.path.join(directory, fn), 'a').close()
+
+            lr = LogReader(directory, file_pattern="*.log")
+            assert isinstance(lr.logfiles(), types.GeneratorType)
+            files = sorted([x for x in lr.logfiles()])
+
+            self.assertEqual(files, sorted([os.path.join(directory, fn) for fn in target_files]))
+
+        finally:
+            shutil.rmtree(directory)
+
+    def test_logfiles_directory_recursive(self):
+        """
+        We should enumerate the matching files from the specified path, without recursively
+        descending into subdirectories
+        """
+        dir_list = ['dir1', 'dir2/dir3']
+
+        file_list = [
+            'file1.log',
+            'file2.log',
+            'dir1/file3.log',
+            'dir2/dir3/file4.log',
+            'other1',
+            'other2',
+            'dir1/other3'
+        ]
+
+        target_files = file_list
+
+        # Create a temporary directory. try/finally ensures that it _WILL_ be deleted
+        directory = tempfile.mkdtemp()
+        try:
+            for d in dir_list:
+                os.makedirs(os.path.join(directory, d))
+            for fn in file_list:
+                # Create an (empty) file
+                open(os.path.join(directory, fn), 'a').close()
+
+            lr = LogReader(directory, recursive=True)
+            assert isinstance(lr.logfiles(), types.GeneratorType)
+            files = sorted([x for x in lr.logfiles()])
+
+            self.assertEqual(files, sorted([os.path.join(directory, fn) for fn in target_files]))
+
+        finally:
+            shutil.rmtree(directory)
+
+    def test_logfiles_directory_recursive_pattern(self):
+        """
+        We should enumerate the matching files from the specified path, without recursively
+        descending into subdirectories
+        """
+        dir_list = ['dir1', 'dir2/dir3']
+
+        file_list = [
+            'file1.log',
+            'file2.log',
+            'dir1/file3.log',
+            'dir2/dir3/file4.log',
+            'other1',
+            'other2',
+            'dir1/other3'
+        ]
+
+        target_files  = [
+            'file1.log',
+            'file2.log',
+            'dir1/file3.log',
+            'dir2/dir3/file4.log'
+        ]
+
+        # Create a temporary directory. try/finally ensures that it _WILL_ be deleted
+        directory = tempfile.mkdtemp()
+        try:
+            for d in dir_list:
+                os.makedirs(os.path.join(directory, d))
+            for fn in file_list:
+                # Create an (empty) file
+                open(os.path.join(directory, fn), 'a').close()
+
+            lr = LogReader(directory, recursive=True, file_pattern="*.log")
+            assert isinstance(lr.logfiles(), types.GeneratorType)
+            files = sorted([x for x in lr.logfiles()])
+
+            self.assertEqual(files, sorted([os.path.join(directory, fn) for fn in target_files]))
+
+        finally:
+            shutil.rmtree(directory)
 
 if __name__ == "__main__":
     unittest.main()
