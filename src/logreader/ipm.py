b835721f6833df4b92795489b9ac580fe8bc76a4
Simon Smart <simon.smart@ecmwf.int>
2016-08-03 13:57:22 +0100

    Start adding parameters to structured IPM reader

--- src/logreader/ipm.py
+++ src/logreader/ipm.py
@@ -51,6 +51,41 @@ class IPMLogReader(LogReader):
     # By default we end up with a whole load of darshan logfiles within a directory.
     label_method = "directory"
 
+    func_mapping = {
+        "MPI_Init": None,
+        "MPI_Init_thread": None,
+        "MPI_Finalize": None,
+        "MPI_Comm_rank": None,
+        "MPI_Comm_size": None,
+        "MPI_Wait": None,
+        "MPI_Waitany": None,
+        "MPI_Buffer_attach": None,
+        "MPI_Buffer_detach": None,
+        "MPI_Barrier": None,
+        "MPI_Comm_group": None,
+        "MPI_Comm_compare": None,
+        "MPI_Comm_dup": None,
+        "MPI_Comm_create": None,
+
+        "MPI_Send": ('mpi_pairwise_count_send', 'mpi_pairwise_bytes_send'),
+        "MPI_Bsend": ('mpi_pairwise_count_send', 'mpi_pairwise_bytes_send'),
+        "MPI_Rsend": ('mpi_pairwise_count_send', 'mpi_pairwise_bytes_send'),
+        "MPI_Ssend": ('mpi_pairwise_count_send', 'mpi_pairwise_bytes_send'),
+        "MPI_Isend": ('mpi_pairwise_count_send', 'mpi_pairwise_bytes_send'),
+        "MPI_Issend": ('mpi_pairwise_count_send', 'mpi_pairwise_bytes_send'),
+        "MPI_Irsend": ('mpi_pairwise_count_send', 'mpi_pairwise_bytes_send'),
+        "MPI_Ibsend": ('mpi_pairwise_count_send', 'mpi_pairwise_bytes_send'),
+
+        "MPI_Recv": ('mpi_pairwise_count_recv', 'mpi_pairwise_bytes_recv'),
+        "MPI_Irecv": ('mpi_pairwise_count_recv', 'mpi_pairwise_bytes_recv'),
+
+        "MPI_Bcast": ('mpi_collective_count', 'mpi_collective_bytes'),
+        "MPI_Gatherv": ('mpi_collective_count', 'mpi_collective_bytes'),
+        "MPI_Allgatherv": ('mpi_collective_count', 'mpi_collective_bytes'),
+        "MPI_Allreduce": ('mpi_collective_count', 'mpi_collective_bytes'),
+        "MPI_Alltoallv": ('mpi_collective_count', 'mpi_collective_bytes')
+    }
+
     def __init__(self, path, **kwargs):
 
         # TODO: Configure the darshan paths (need darshan-parser)
@@ -61,12 +96,57 @@ class IPMLogReader(LogReader):
 
         super(IPMLogReader, self).__init__(path, **kwargs)
 
+    def parse_calltable(self, calltable):
+
+        assert calltable is not None
+
+        sections = calltable.findall('section')
+        for section in sections:
+
+            module = section.get('module')
+            entries = section.findall('entry')
+            assert len(entries) == int(section.get('nentries'))
+            for entry in entries:
+                print "{} : {}".format(module, entry.get('name'))
+
+                if entry.get('name') not in self.func_mapping:
+                    raise Exception("Oh no... {} {}".format(module, entry.get('name')))
+
+
+    def parse_tasks(self, tasks):
+
+        assert len(tasks) > 0
+
+        for task in tasks:
+
+            assert int(task.get('mpi_size')) == len(tasks)
+            assert int(task.find('job').get('ntasks')) == len(tasks)
+
+            regions_container = task.find('regions')
+            nregions = int(regions_container.get('n'))
+
+            regions = regions_container.findall('region')
+            assert len(regions) == nregions
+
+            for region in regions:
+
+                funcs = region.findall('func')
+                for func in funcs:
+                    print "F: {} {} {}".format(func.get('name'), func.get('count'), func.get('bytes'))
+
     def read_log(self, filename, suggested_label):
         """
         Read a darshan log!
         """
-        print "LOG: {}".format(filename)
         root = ET.parse(filename).getroot()
+
+        # Look at the available opts
+        assert root.tag == "ipm_job_profile"
+
+        self.parse_calltable(root.find('calltable'))
+
+        self.parse_tasks(root.findall('task'))
+
         return []
 
     def read_logs_generator(self):
