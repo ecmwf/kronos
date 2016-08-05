a3f416799514339dce200a2fab8ee1e3a8f80b71
Simon Smart <simon.smart@ecmwf.int>
2016-08-05 18:04:22 +0100

    Test ModelJob.is_valid()

--- src/tests/test_jobs.py
+++ src/tests/test_jobs.py
@@ -4,10 +4,31 @@ import unittest
 from jobs import ModelJob
 
 
-class ModelJobTest(unittest.TetsCase):
+class ModelJobTest(unittest.TestCase):
 
     def test_initialisation(self):
         self.assertFalse(True)
+
+    def test_is_valid(self):
+        """
+        There are some things that are required. Check these things here!
+        """
+        job = ModelJob()
+        self.assertFalse(job.is_valid())
+
+        # If all of the required arguments are supplied, this should result in a valid job
+        valid_args = {
+            'time_start': 0,
+            'ncpus': 1,
+            'nnodes': 1
+        }
+        self.assertTrue(ModelJob(**valid_args).is_valid())
+
+        # If any of the supplied arguments are missing, this should invalidate things
+        for k in valid_args.keys():
+            invalid_args = valid_args.copy()
+            del invalid_args[k]
+            self.assertTrue(ModelJob(**valid_args).is_valid())
 
 
 class IngestedJobTest(unittest.TestCase):
