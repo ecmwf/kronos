112c1bb083a65879a28d45520af7ad0e3d8f7d15
Simon Smart <simon.smart@ecmwf.int>
2016-08-05 15:49:03 +0100

    Test job aggregation in IPMLogReader

--- src/tests/logreader/test_ipm_logreader.py
+++ src/tests/logreader/test_ipm_logreader.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import unittest
+import types
 
-from logreader.ipm import IPMTaskInfo
+from logreader.ipm import IPMTaskInfo, IPMLogReader
 
 
 class IPMTaskInfoTest(unittest.TestCase):
@@ -75,6 +76,19 @@ class IPMIngestedJobTest(unittest.TestCase):
         self.assertFalse(True)
 
 
+class FakeJob(object):
+    """
+    This is a mocked-up ingested job. It only exists as a file-level class so that it is picklable, which
+    makes it work happily with the multiprocessing module.
+    """
+    def __init__(self, name, label):
+        self.names = [name]
+        self.label = label
+
+    def aggregate(self, other):
+        self.names += other.names
+
+
 class IPMLogReaderTest(unittest.TestCase):
 
     def test_initialisation(self):
@@ -82,6 +96,32 @@ class IPMLogReaderTest(unittest.TestCase):
         Check that we have sensible (overridable) defaults. N.b. inherits from LogReader
         """
         self.assertFalse(True)
+
+    def test_read_logs(self):
+        """
+        Should return a generator of the output of read_log depending on the results of logfiles.
+
+        For IPM, we get multiple logfiles per job (as one is produces per command that is contained in the
+        submit script). Assume that thee are grouped per directory, and aggregate is called on the job class.
+        """
+        class LogReaderLocal(IPMLogReader):
+            def logfiles(self):
+                for file_path in ['dir1/file1', 'dir1/file2', 'dir1/file3', 'dir2/file4']:
+                    yield file_path
+
+            def read_log(self, filename, suggested_label):
+                return [FakeJob(filename, suggested_label)]
+
+        lr = LogReaderLocal('test-path')
+
+        logs = lr.read_logs()
+        self.assertIsInstance(logs, types.GeneratorType)
+
+        logs = list(logs)
+        self.assertEqual(len(logs), 2)
+
+        self.assertEqual(logs[0].names, ['dir1/file1', 'dir1/file2', 'dir1/file3'])
+        self.assertEqual(logs[1].names, ['dir2/file4'])
 
 
 if __name__ == "__main__":
