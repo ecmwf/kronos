1a0781a16cfe8caa07735ac166dcd588c3b978f9
Simon Smart <simon.smart@ecmwf.int>
2016-09-06 09:52:10 +0100

    Enable module discovery manually

--- dev/run_tests.sh
+++ dev/run_tests.sh
@@ -7,4 +7,4 @@ export PATH=${bamboo_working_directory}/miniconda/bin:${PATH}
 source activate test_env
 
 cd ${bamboo_working_directory}/src
-python ${bamboo_working_directory}/miniconda/envs/test_env/lib/python2.7/site-packages/pytest.py --junitxml="${bamboo_working_directory}/test_output.xml" tests
+PYTHONPATH=${bamboo_working_directory}/src python ${bamboo_working_directory}/miniconda/envs/test_env/lib/python2.7/site-packages/pytest.py --junitxml="${bamboo_working_directory}/test_output.xml" tests
