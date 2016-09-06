445f903a73aecc0771257352219a2c39ffa2a6b2
Simon Smart <simon.smart@ecmwf.int>
2016-09-06 10:55:02 +0100

    Be more explicit abotu what should be linted.

--- dev/run_lint.sh
+++ dev/run_lint.sh
@@ -5,4 +5,4 @@
 export PATH=${bamboo_working_directory}/miniconda/bin:${PATH}
 source activate test_env
 
-find ${bamboo_working_directory} -type f -name "*.py" -print0 | xargs -0 -n1 pyflakes;
+find ${bamboo_working_directory}/src -type f -name "*.py" -print0 | xargs -0 -n1 pyflakes;
