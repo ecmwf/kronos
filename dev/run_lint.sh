14879af039da28f3ac6a6b42639e9a53d76befab
Simon Smart <simon.smart@ecmwf.int>
2016-09-06 10:38:13 +0100

    Activate the conda environment before linting

--- dev/run_lint.sh
+++ dev/run_lint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-#!/bin/sh
+
+# Get access to the installed python environment
 
 export PATH=${bamboo_working_directory}/miniconda/bin:${PATH}
+source activate test_env
 
 find ${bamboo_working_directory} -type f -name "*.py" -print0 | xargs -0 -n1 pyflakes;
