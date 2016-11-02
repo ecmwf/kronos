1145602ca7b3a313446604ea23dbe919ca50458d
Antonino Bonanni <antonino.bonanni@ecmwf.int>
2016-11-02 10:46:04 +0000

    some more plots in the postprocessing routine..

--- src/plugins/plugin_base.py
+++ src/plugins/plugin_base.py
@@ -1,13 +1,13 @@
 import os
 import glob
 import json
-import pprint
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
 from time_signal import signal_types
-from kronos_tools.print_colour import print_colour
+# from kronos_tools.print_colour import print_colour
+import logreader
 
 
 class PluginBase(object):
@@ -36,21 +36,15 @@ class PluginBase(object):
 
         config_pp = self.config.post_process
 
-        # raise NotImplementedError("Must use derived class. Call clustering.factory")
-        pp = pprint.PrettyPrinter(indent=4)
-
+        # ---------------------- read sa json's (run input) ------------------------
         # list of json files in folder..
         json_files = glob.glob(os.path.join(os.path.realpath(config_pp['dir_sa_run_input']), "*.json"))
         if not json_files:
-            raise OSError("no json files of synth apps found in {}".format(config_pp['dir_sa_run_input']))
+            raise OSError("no json files of synthetic apps found in {}".format(config_pp['dir_sa_run_input']))
 
         json_files.sort()
-
-        # ============== reformat the sa dictionary for easy use.. ==================
         sa_data_list = []
         for file_name in json_files:
-
-            print_colour("green", "reading file {}".format(file_name))
 
             # Initialize the sa data
             sa_data = {}
@@ -82,14 +76,9 @@ class PluginBase(object):
 
             sa_data_list.append(sa_data)
 
-        # # print out the sa list..
-        # for sa in sa_data_list:
-        #     pp.pprint(sa)
-
-        print "--------------------- printing summary -------------------------"
-        print "N synthetic apps = {}".format(len(sa_data_list))
-
-        print "Sums of all quantities:"
+        print "---------------------------------------------------------"
+        print "Total number of synthetic apps = {}".format(len(sa_data_list))
+        print "Sums REQUESTED quantities:"
         sums_sig = {el: 0 for el in signal_types}
         for sa in sa_data_list:
             for tt in sa['frames'].keys():
@@ -98,63 +87,122 @@ class PluginBase(object):
         for ss in sums_sig.keys():
             print "    {} = {}".format(ss, sums_sig[ss])
 
+        # --------------- check output if requested -----------------
+        if (config_pp["post_process_scope"] == "output") or (config_pp["post_process_scope"] == "both"):
+            sa_out_dataset = logreader.ingest_data("allinea", config_pp['dir_sa_run_output'])
+            allinea_jsons_sums = {el: 0 for el in signal_types}
+            for sa in sa_out_dataset.joblist:
+                for tt in sa.timesignals.keys():
+                    allinea_jsons_sums[tt] += sum(sa.timesignals[tt].yvalues)
 
-        # ========================= plot ================================
-        print " sums of the model before the generation of the synthetic apps "
+            total_exe_time = max(sa.time_end for sa in sa_out_dataset.joblist) - min(
+                sa.time_start for sa in sa_out_dataset.joblist)
+
+            print "---------------------------------------------------------"
+            print "Sums RUN quantities:"
+            print "Total execution time: {}".format(total_exe_time)
+            for ss in allinea_jsons_sums.keys():
+                print "    {} = {}".format(ss, allinea_jsons_sums[ss])
+
+        # ========================= summary plots ================================
         with open(os.path.join(self.config.dir_output, 'sa_workload_pickle'), 'r') as f:
             synth_wl = pickle.load(f)
         total_metrics = synth_wl.total_metrics_dict()
 
         # ---------- make plot ----------
-        fig_size = (16, 4)
+        fig_size = (16, 8)
         plt.figure(101, figsize=fig_size)
-        plt.figure(101)
         width = 0.8
+        id_plot = 0
 
-        plt.subplot(2,1,1)
-        plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.1)
-        xx = np.arange(len(total_metrics.keys()))
-        plt.bar(xx, np.asarray([v for k,v in total_metrics.items()]),
-                width,
-                color='grey',
-                label='unscaled')
-        plt.bar(xx, np.asarray([v for k,v in sums_sig.items()]),
-                width,
-                color='red',
-                label='synthetic apps')
-        plt.xlabel('metrics')
-        plt.ylabel('sums over all jobs')
-        plt.yscale('log')
-        plt_hdl = plt.gca()
-        plt_hdl.set_xticks([i+width/2. for i in xx])
-        plt_hdl.set_xticklabels([k for k,v in sums_sig.items()])
-        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        if (config_pp["post_process_scope"] == "input") or (config_pp["post_process_scope"] == "output"):
+            n_plots = 2
+        elif config_pp["post_process_scope"] == "both":
+            n_plots = 4
 
-        plt.subplot(2,1,2)
-        plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.1)
-        xx = np.arange(len(total_metrics.keys()))
-        plt.bar(xx, np.asarray([v for k,v in total_metrics.items()]),
-                width,
-                color='grey',
-                label='unscaled')
-        plt.bar(xx, np.asarray([v/self.config.plugin['tuning_factors'][k] for k,v in sums_sig.items()]),
-                width,
-                color='red',
-                label='synthetic apps')
-        plt.xlabel('metrics')
-        plt.ylabel('sums over all jobs')
-        plt.yscale('log')
-        plt_hdl = plt.gca()
-        plt_hdl.set_xticks([i+width/2. for i in xx])
-        plt_hdl.set_xticklabels([k for k,v in sums_sig.items()])
-        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        if (config_pp["post_process_scope"] == "input") or (config_pp["post_process_scope"] == "both"):
 
+            id_plot += 1
+            plt.subplot(n_plots, 1, id_plot)
+            plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.1)
+            xx = np.arange(len(total_metrics.keys()))
+            plt.bar(xx, np.asarray([v for k,v in total_metrics.items()]),
+                    width,
+                    color='grey',
+                    label='unscaled')
+            plt.bar(xx, np.asarray([v for k,v in sums_sig.items()]),
+                    width,
+                    color='red',
+                    label='synthetic apps')
+            plt.xlabel('metrics')
+            plt.ylabel('sums over all jobs')
+            plt.yscale('log')
+            plt_hdl = plt.gca()
+            plt_hdl.set_xticks([i+width/2. for i in xx])
+            plt_hdl.set_xticklabels([k for k,v in sums_sig.items()])
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
+            id_plot += 1
+            plt.subplot(n_plots, 1, id_plot)
+            plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.1)
+            xx = np.arange(len(total_metrics.keys()))
+            plt.bar(xx, np.asarray([v for k,v in total_metrics.items()]),
+                    width,
+                    color='grey',
+                    label='unscaled')
+            plt.bar(xx, np.asarray([v/self.config.plugin['tuning_factors'][k] for k,v in sums_sig.items()]),
+                    width,
+                    color='red',
+                    label='synthetic apps')
+            plt.xlabel('metrics')
+            plt.ylabel('sums over all jobs')
+            plt.yscale('log')
+            plt_hdl = plt.gca()
+            plt_hdl.set_xticks([i+width/2. for i in xx])
+            plt_hdl.set_xticklabels([k for k,v in sums_sig.items()])
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+        if (config_pp["post_process_scope"] == "output") or (config_pp["post_process_scope"] == "both"):
+
+            id_plot += 1
+            plt.subplot(n_plots, 1, id_plot)
+            plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.1)
+            xx = np.arange(len(total_metrics.keys()))
+            plt.bar(xx, np.asarray([v for k, v in sums_sig.items()]), width / 2.0, color='red',
+                    label='input synthetic apps')
+            plt.bar(xx + width / 2.0, np.asarray([v for k, v in allinea_jsons_sums.items()]), width / 2.0, color='blue',
+                    label='run synthetic apps')
+            plt.xlabel('metrics')
+            plt.ylabel('sums over all jobs')
+            plt.yscale('log')
+            plt_hdl = plt.gca()
+            plt_hdl.set_xticks([i + width / 2. for i in xx])
+            plt_hdl.set_xticklabels([k for k, v in sums_sig.items()])
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+            id_plot += 1
+            plt.subplot(n_plots, 1, id_plot)
+            plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.1)
+            xx = np.arange(len(total_metrics.keys()))
+            plt.bar(xx,
+                    np.asarray([v for k, v in sums_sig.items()]) - np.asarray([v for k, v in allinea_jsons_sums.items()]),
+                    width,
+                    color='green',
+                    label='diff req-run')
+
+            plt.xlabel('metrics')
+            plt.ylabel('sums over all jobs')
+            plt.yscale('log')
+            plt_hdl = plt.gca()
+            plt_hdl.set_xticks([i + width / 2. for i in xx])
+            plt_hdl.set_xticklabels([k for k, v in sums_sig.items()])
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
         # plt.savefig(out_dir + '/' + "hist_pg16.png")
         # plt.close(101)
         plt.show()
-        #===============================================================
+        # ===============================================================
+
 
 
 
