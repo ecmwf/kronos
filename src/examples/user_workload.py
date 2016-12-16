corrected a mistake in the calculation of the generated time stamps of the synth apps

src/examples/user_workload.py
@@ -344,7 +344,13 @@ def user_workload():
                     'labels': clusters_labels,
                     }]
 
-    sapps_generator = generator.SyntheticWorkloadGenerator(config_generator, clusters_dict)
+    # sapps_generator = generator.SyntheticWorkloadGenerator(config_generator, clusters_dict)
+
+    global_t0 = min(j.time_start for cl in clusters_dict for j in cl['jobs_for_clustering'])
+    global_tend = max(j.time_start for cl in clusters_dict for j in cl['jobs_for_clustering'])
+    sapps_generator = generator.SyntheticWorkloadGenerator(config_generator, clusters_dict, global_t0, global_tend)
+
+
     modelled_sa_jobs = sapps_generator.generate_synthetic_apps()
 
     # ------------ plot real/synthetic job distribution ---------------
@@ -359,7 +365,7 @@ def user_workload():
     print "xedge_bins", xedge_bins
     print "tt_hist", tt_hist[0]
 
-    #synthetic start times
+    # synthetic start times
     sa_hist = np.histogram(np.asarray([j.time_start for j in modelled_sa_jobs]), bins=xedge_bins)
     print "sa_hist", sa_hist[0]
 

src/generator.py
@@ -3,6 +3,7 @@ from jobs import ModelJob
 from kronos_tools.print_colour import print_colour
 import numpy as np
 
+from kronos_tools.time_pdf import equiv_time_pdf_exact, equiv_time_pdf
 from synthetic_app import SyntheticApp
 from time_signal import TimeSignal, time_signal_names
 
@@ -25,9 +26,12 @@ class SyntheticWorkloadGenerator(object):
                              "total_submit_interval",
                              ]
 
-    def __init__(self, config_generator, clusters):
+    def __init__(self, config_generator, clusters, global_t0, global_tend, n_bins_for_pdf=None):
         self.config_generator = config_generator
         self.clusters = clusters
+        self.global_t0 = global_t0
+        self.global_tend = global_tend
+        self.n_bins_for_pdf = n_bins_for_pdf
 
     def check_config(self):
         """
@@ -58,47 +62,25 @@ class SyntheticWorkloadGenerator(object):
         Generate jobs matching PDF of jobs in the time window of the workload
         :return:
         """
-        # raise NotImplementedError("not implemented!")
 
         generated_sa_from_all_wl = []
+        n_bins_for_pdf = self.n_bins_for_pdf if self.n_bins_for_pdf else 20
 
         # generate a synthetic workload for each cluster of jobs
         for cluster in self.clusters:
 
-            # calculate the submit rate from the selected workload
             start_times = [j.time_start for j in cluster['jobs_for_clustering']]
-            real_submit_rate = float(len(start_times)) / (max(start_times) - min(start_times))
-            requested_submit_rate = real_submit_rate * self.config_generator['submit_rate_factor']
-            n_modelled_jobs = int(requested_submit_rate * self.config_generator['total_submit_interval'])
+            start_times_vec_sa, _, _ = equiv_time_pdf(start_times,
+                                                      self.global_t0,
+                                                      self.global_tend,
+                                                      self.config_generator['total_submit_interval'],
+                                                      self.config_generator['submit_rate_factor'],
+                                                      n_bins_for_pdf)
 
-            if not n_modelled_jobs:
-                out_str = 'Low submit rate! '
-                out_str += 'real={} jobs/sec '.format(real_submit_rate)
-                out_str += 'requested={} jobs/sec '.format(requested_submit_rate)
-                out_str += 'n of jobs {} '.format(n_modelled_jobs)
-                out_str += '=> Number of jobs will be set to *1*'
-                print_colour("orange", out_str)
-                n_modelled_jobs = 1
-
-            # create random vector of cluster indices and start times consistent with the PDF of the real start times
+            # Random vector of cluster indices
+            n_modelled_jobs = len(start_times_vec_sa)
             np.random.seed(self.config_generator['random_seed'])
             vec_clust_indexes = np.random.randint(cluster['cluster_matrix'].shape[0], size=n_modelled_jobs)
-
-            # find the PDF of jobs start times
-            bins = np.linspace(0, self.config_generator['total_submit_interval'], 50)
-
-            # normalize the vector of start time to make it in [0, max T submit]
-            start_times_vec = np.asarray(start_times)
-            start_times_norm = (start_times_vec-min(start_times_vec))/(max(start_times_vec)-min(start_times_vec))
-            start_times_norm *= self.config_generator['total_submit_interval']
-
-            # then calculate the PDF
-            time_start_pdf, time_start_bin_edges = np.histogram(start_times_norm, bins, density=False)
-            time_start_pdf_norm = time_start_pdf/float(sum(time_start_pdf))
-            time_start_bin_mid = (time_start_bin_edges[:-1]+time_start_bin_edges[1:])/2.0
-
-            # then calculate a random distribution of time start from the provided PDF
-            start_times_vec_sa = np.random.choice(time_start_bin_mid, p=time_start_pdf_norm, size=n_modelled_jobs)
 
             # loop over the clusters and generates jos as needed
             generated_model_jobs = []
@@ -124,7 +106,6 @@ class SyntheticWorkloadGenerator(object):
 
             # --- then create the synthetic apps from the generated model jobs --
             modelled_sa_jobs = self.model_jobs_to_sa(generated_model_jobs, cluster['source-workload'])
-
             generated_sa_from_all_wl.extend(modelled_sa_jobs)
 
         return generated_sa_from_all_wl
@@ -136,36 +117,20 @@ class SyntheticWorkloadGenerator(object):
         """
 
         generated_sa_from_all_wl = []
+        n_bins_for_pdf = self.n_bins_for_pdf if self.n_bins_for_pdf else 20
 
         # generate a synthetic workload for each cluster of jobs
         for cluster in self.clusters:
 
-            # calculate the submit rate from the selected workload
             start_times = [j.time_start for j in cluster['jobs_for_clustering']]
-            # real_submit_rate = float(len(start_times)) / (max(start_times) - min(start_times))
+            start_times_vec_sa, _, _ = equiv_time_pdf_exact(start_times,
+                                                            self.global_t0,
+                                                            self.global_tend,
+                                                            self.config_generator['total_submit_interval'],
+                                                            self.config_generator['submit_rate_factor'],
+                                                            n_bins_for_pdf)
 
-            # find the PDF of jobs start times
-            sa_bins = np.linspace(0, self.config_generator['total_submit_interval'], 15)
-
-            # normalize the vector of start time to make it in [0, max T submit]
-            start_times_vec = np.asarray(start_times)
-            start_times_norm = (start_times_vec - min(start_times_vec)) / (max(start_times_vec) - min(start_times_vec))
-            start_times_norm *= self.config_generator['total_submit_interval']
-
-            # then calculate the PDF
-            time_start_pdf, time_start_bin_edges = np.histogram(start_times_norm, sa_bins, density=False)
-
-            # then calculate an "exact" distribution of time start from the provided PDF
-            sa_time_ratio = self.config_generator['submit_rate_factor']
-            start_times_vec_sa = np.asarray([])
-            for bb in range(len(time_start_pdf)):
-                y_min = sa_bins[bb]
-                y_max = sa_bins[bb + 1]
-                n_sa_bin = int(time_start_pdf[bb] * sa_time_ratio)
-                random_y_values = y_min + np.random.rand(n_sa_bin) * (y_max-y_min)
-                start_times_vec_sa = np.append(start_times_vec_sa, random_y_values)
-
-            # create random vector of cluster indices and start times consistent with the PDF of the real start times
+            # Random vector of cluster indices
             n_modelled_jobs = len(start_times_vec_sa)
             np.random.seed(self.config_generator['random_seed'])
             vec_clust_indexes = np.random.randint(cluster['cluster_matrix'].shape[0], size=n_modelled_jobs)
@@ -198,7 +163,6 @@ class SyntheticWorkloadGenerator(object):
             generated_sa_from_all_wl.extend(modelled_sa_jobs)
 
         return generated_sa_from_all_wl
-
 
     def match_job_rate(self):
         """

src/tests/workload/test_generator.py
@@ -76,24 +76,8 @@ class GeneratorTests(unittest.TestCase):
                                     )
 
         # create a matrix from jobs timesignals..
-        # TODO: this should be done inside the model jobs..
         n_ts_bins = 2
-        input_jobs_matrix = np.zeros((len(input_jobs), len(time_signal.signal_types) * n_ts_bins))
-        non_value_flag = -999
-
-        # loop over jobs and fill the matrix as appropriate
-        for cc, job in enumerate(input_jobs):
-
-            row = []
-            for tsk in time_signal.time_signal_names:
-                ts = job.timesignals[tsk]
-                if ts is not None:
-                    xvals, yvals = ts.digitized(n_ts_bins)
-                    row.extend(yvals)
-                else:
-                    row.extend([non_value_flag for vv in range(0, n_ts_bins)])
-
-            input_jobs_matrix[cc, :] = np.asarray(row)
+        input_jobs_matrix = test_workload.jobs_to_matrix(n_ts_bins)
 
         clusters = {
                     'source-workload': test_workload.tag,
@@ -102,7 +86,9 @@ class GeneratorTests(unittest.TestCase):
                     'labels': [1 for job in input_jobs],
                     }
 
-        sapps_generator = generator.SyntheticWorkloadGenerator(config_generator, [clusters])
+        global_t0 = min(j.time_start for j in clusters['jobs_for_clustering'])
+        global_tend = max(j.time_start for j in clusters['jobs_for_clustering'])
+        sapps_generator = generator.SyntheticWorkloadGenerator(config_generator, [clusters], global_t0, global_tend)
         modelled_sa_jobs = sapps_generator.generate_synthetic_apps()
 
         # check that the produced sapps are produced from one of the clusters in the matrix..
@@ -111,71 +97,99 @@ class GeneratorTests(unittest.TestCase):
                         for ts in time_signal.time_signal_names
                         )
 
-    # def test_dict_override(self):
-    #
-    #     # existing and not existing paths
-    #     existing_path = os.getcwd()
-    #     obscure_path = '.__$obscure-nonexistent@'
-    #     self.assertFalse(os.path.exists(os.path.join(os.getcwd(), obscure_path)))
-    #
-    #     # We can override each of the parameters
-    #     config_dict = {
-    #         'dir_input': existing_path,
-    #         'dir_output': existing_path,
-    #     }
-    #     cfg = Config(config_dict=config_dict)
-    #     self.assertEqual(cfg.dir_input, existing_path)
-    #     self.assertEqual(cfg.dir_output, existing_path)
-    #     # self.assertEqual(cfg.profile_sources, [1, 2, 3, 4])
-    #
-    #     # Unexpected parameters throw exceptions
-    #     config_dict['unexpected'] = 'parameter'
-    #     self.assertRaises(ConfigurationError, lambda: Config(config_dict=config_dict))
-    #
-    # def test_path_override(self):
-    #     """
-    #     Test the overrides, but put the data into a file
-    #     """
-    #
-    #     # existing and not existing paths
-    #     existing_path = os.getcwd()
-    #     obscure_path = '.__$obscure-nonexistent@'
-    #     self.assertFalse(os.path.exists(os.path.join(os.getcwd(), obscure_path)))
-    #
-    #     # We should get an exception if we throw idiotic stuff in
-    #     self.assertRaises(IOError, lambda: Config(config_path='.__$idiotic-nonexistent@'))
-    #
-    #     # An empty override dictionary does nothing
-    #     with tempfile.NamedTemporaryFile() as f:
-    #         f.write("{}")
-    #         f.flush()
-    #         self.assertRaises(ConfigurationError, lambda: Config(config_dict=f.name))
-    #
-    #     # We can override each of the parameters
-    #     # n.b. Test the comment handling in the  JSON parser
-    #     obscure_path = os.path.join(os.getcwd(), '.__$obscure-nonexistent@')
-    #     self.assertFalse(os.path.exists(obscure_path))
-    #     with tempfile.NamedTemporaryFile() as f:
-    #         f.write("""{{
-    #             "dir_input": "{}",
-    #             #"unknown": "parameter",
-    #             "dir_output": "{}"
-    #         }}""".format(existing_path, existing_path))
-    #         f.flush()
-    #         cfg = Config(config_path=f.name)
-    #     self.assertEqual(cfg.dir_input, existing_path)
-    #     self.assertEqual(cfg.dir_output, existing_path)
-    #     # self.assertEqual(cfg.profile_sources, [1, 2, 3, 4])
-    #
-    #     # Unexpected parameters throw exceptions
-    #     with tempfile.NamedTemporaryFile() as f:
-    #         f.write("""{{
-    #             "dir_input": "abcdef",
-    #             "dir_output": "{}",
-    #             "unknown": "{}"
-    #         }}""".format(existing_path, existing_path))
-    #         f.flush()
-    #         self.assertRaises(ConfigurationError, lambda: Config(config_path=f.name))
+    def test_generator_job_schedule(self):
+        """
+        test the job schedule generation
+        :return:
+        """
+
+        # If all of the required arguments are supplied, this should result in a valid job
+        ts_complete_set = {tsk: time_signal.TimeSignal.from_values(tsk, [0., 0.1, 0.3], [1., 999., 666.])
+                           for tsk in time_signal.time_signal_names}
+
+        valid_args = {
+            'time_start': 0.1,
+            'duration': 0.2,
+            'ncpus': 1,
+            'nnodes': 1,
+            'timesignals': ts_complete_set
+        }
+
+        # create 20 jobs from these time signals
+        input_jobs = [ModelJob(**valid_args) for _ in range(20)]
+
+        # diversify the time start..
+        for jj, job in enumerate(input_jobs):
+            if jj <= 5:
+                job.time_start += 10.
+            elif 5 < jj < 10:
+                job.time_start += 20.
+            else:
+                job.time_start += 30.
+
+        for job in input_jobs:
+            self.assertTrue(job.is_valid())
+
+        # test schedule of sa when submit_rate_factor == 1
+        config_generator = {
+            "type": "match_job_pdf_exact",
+            "random_seed": 0,
+            "tuning_factors": {
+                "kb_collective": 1e-0,
+                "n_collective": 1e-0,
+                "kb_write": 1e-3,
+                "n_pairwise": 10e-0,
+                "n_write": 1e-2,
+                "n_read": 10e-2,
+                "kb_read": 1e-2,
+                "flops": 500e-0,
+                "kb_pairwise": 10e-0
+            },
+            "submit_rate_factor": 1.0,
+            "synthapp_n_cpu": 2,
+            "synthapp_n_nodes": 1,
+            "synthapp_n_frames": 10,
+            "total_submit_interval": max([j.time_start for j in input_jobs])
+        }
+
+        # create a workload with 5 model jobs
+        test_workload = WorkloadData(
+            jobs=input_jobs,
+            tag='test_wl'
+        )
+
+        # create a matrix from jobs timesignals..
+        n_ts_bins = 2
+        input_jobs_matrix = test_workload.jobs_to_matrix(n_ts_bins)
+
+        clusters = {
+            'source-workload': test_workload.tag,
+            'jobs_for_clustering': test_workload.jobs,
+            'cluster_matrix': input_jobs_matrix[0, :].reshape(1, input_jobs_matrix.shape[1]),
+            'labels': [1 for _ in input_jobs],
+        }
+
+        global_t0 = min(j.time_start for j in clusters['jobs_for_clustering'])
+        global_tend = max(j.time_start for j in clusters['jobs_for_clustering'])
+        sapps_generator = generator.SyntheticWorkloadGenerator(config_generator, [clusters], global_t0, global_tend)
+
+        modelled_sa_jobs = sapps_generator.generate_synthetic_apps()
+
+        xedge_bins = np.linspace(0, max([job.time_start for job in input_jobs]), 10)
+
+        tt_hist, _ = np.histogram(np.asarray([job.time_start for job in input_jobs]), bins=xedge_bins)
+        sa_hist, _ = np.histogram(np.asarray([job.time_start for job in modelled_sa_jobs]), bins=xedge_bins)
+
+        # print "[job.time_start for job in input_jobs]", [job.time_start for job in input_jobs]
+        # print "[job.time_start for job in modelled_sa_jobs]", [job.time_start for job in modelled_sa_jobs]
+        # print "tt_hist", tt_hist
+        # print "sa_hist", sa_hist
+
+        # check that the produced sapps are produced from one of the clusters in the matrix..
+        self.assertTrue(any(np.equal(input_jobs_matrix, job.timesignals[ts].yvalues).all(1))
+                        for job in modelled_sa_jobs
+                        for ts in time_signal.time_signal_names
+                        )
 
 
 if __name__ == "__main__":