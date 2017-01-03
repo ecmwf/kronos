db58d48a2bc50e0b6622675c85b3556d456e8c99
Simon Smart <simon.smart@ecmwf.int>
2017-01-03 12:39:32 +0000

    Equality testing for ProfileFormat

--- src/io/profile_format.py
+++ src/io/profile_format.py
@@ -10,6 +10,7 @@ import time_signal
 
 import strict_rfc3339
 
+from datetime import datetime
 import json
 import jsonschema
 import os
@@ -22,14 +23,54 @@ class ProfileFormat(object):
     kpf_version = 1
     kpf_magic = "KRONOS-KPF-MAGIC"
 
-    def __init__(self, model_jobs=[]):
-        self.profiled_jobs = [self.parse_model_job(m) for m in model_jobs]
+    def __init__(self, model_jobs=None, json_jobs=None, created=None, uid=None):
+
+        # We either initialise from model jobs, or from processed json data
+        assert (model_jobs is not None) != (json_jobs is not None)
+        if model_jobs:
+            self.profiled_jobs = [self.parse_model_job(m) for m in model_jobs]
+        else:
+            self.profiled_jobs = json_jobs
+
+        self.created = created
+        self.uid = uid
 
     def __unicode__(self):
-        return "KPF(njobs={})".format(len(self.profiled_jobs))
+        return "KronosProfileFormat(njobs={})".format(len(self.profiled_jobs))
 
     def __str__(self):
         return unicode(self).encode('utf-8')
+
+    def __eq__(self, other):
+        """
+        Are two profiles the same?
+        """
+        # We don't test the UID/created timestamp. Not interesting. Only care about the data.
+
+        if len(self.profiled_jobs) != len(other.profiled_jobs):
+            return False
+
+        for j1, j2 in zip(self.profiled_jobs, other.profiled_jobs):
+            if (j1['time_end'] != j2['time_end'] or
+                    j1['time_start'] != j2['time_start'] or
+                    j1['duration'] != j2['duration'] or
+                    j1['ncpus'] != j2['ncpus'] or
+                    j1['nnodes'] != j2['nnodes'] or
+                    j1['label'] != j2['label']):
+                return False
+
+            if (j1.get('time_series', None) is not None) != (j2.get('time_series', None) is not None):
+                return False
+
+            if j1.get('time_series', None) is not None:
+                for name, values in j1['time_series'].iteritems():
+                    if name not in j2['time_series']:
+                        return False
+                    if (values.get('times', None) != j2['time_series'][name].get('times', None) or
+                            values.get('values', None) != j2['time_series'][name].get('values', None)):
+                        return False
+
+        return True
 
     @staticmethod
     def parse_model_job(model_job):
@@ -65,9 +106,11 @@ class ProfileFormat(object):
         data = json.load(f)
         ProfileFormat.validate_json(data)
 
-        pf = ProfileFormat()
-        pf.profiled_jobs = data['profiled_jobs']
-        return pf
+        return ProfileFormat(
+            json_jobs=data['profiled_jobs'],
+            created=datetime.fromtimestamp(strict_rfc3339.rfc3339_to_timestamp(data['created'])),
+            uid=data['uid']
+        )
 
     @staticmethod
     def from_filename(filename):
@@ -136,8 +179,6 @@ if __name__ == "__main__":
         ds = pickle.load(f)
 
     model_jobs = ds.model_jobs()
-    print model_jobs
-
 
     with open('output.kpf', 'w') as f:
 
@@ -146,141 +187,7 @@ if __name__ == "__main__":
         pf.write(f)
 
     with open('output.kpf', 'r') as f:
-        pf = ProfileFormat.from_file(f)
-        print pf
+        pf2 = ProfileFormat.from_file(f)
+        print pf2
 
-#class KPFFileHandler(object):
-#    """
-#    Class that encapsulates some of the operations for a kpf file
-#    """
-#    def __init__(self):
-#        pass
-#
-#    @staticmethod
-#    def save_kpf(workloads, kpf_filename=None):
-#        """
-#        Export a kpf file
-#        :param workloads: list of workloads
-#        :param kpf_filename:
-#        :return:
-#        """
-#
-#        # if the workloads is only one workload, translate it to a list
-#        if isinstance(workloads, WorkloadData):
-#            workloads = [workloads]
-#        else:
-#            assert all(isinstance(wl, WorkloadData) for wl in workloads)
-#
-#        print_colour.print_colour('white', 'exporting kpf file: {}'.format(kpf_filename))
-#
-#        if kpf_filename:
-#
-#            kpf_data = []
-#
-#            if not kpf_filename.endswith('.kpf'):
-#                print("extension .ksp will be appended")
-#                kpf_filename += '.kpf'
-#
-#            for wl in workloads:
-#
-#                # collect data from jobs in the dataset
-#                wl_jobs_json = []
-#                for job in wl.jobs:
-#
-#                    job_entry_data = OrderedDict([
-#                                                ('job_name', job.job_name),
-#                                                ('user_name', job.user_name),
-#                                                ('cmd_str', job.cmd_str),
-#                                                ('queue_name', job.queue_name),
-#                                                ('time_queued', job.time_queued),
-#                                                ('time_start', job.time_start),
-#                                                ('duration', job.duration),
-#                                                ('ncpus', job.ncpus),
-#                                                ('nnodes', job.nnodes),
-#                                                ('stdout', job.stdout),
-#                                                ('label', job.label),
-#                                                ])
-#
-#                    # # automatically creates a dictionary with all the class attributes..
-#                    # # excluding callable functions and properties
-#                    # job_entry_data = {k: v for k, v in ModelJob.__dict__.items()
-#                    #                   if not k.startswith('__') and not callable(v) and not isinstance(v, property)}
-#
-#                    # append timeseries signals
-#                    timesignals_dict = {}
-#                    for tsk, tsv in job.timesignals.items():
-#                        if tsv:
-#                            timesignals_dict[tsk] = OrderedDict([
-#                                ('xvalues', list(tsv.xvalues)),
-#                                ('yvalues', list(tsv.yvalues)),
-#                            ])
-#
-#                    job_entry_data['timesignals'] = timesignals_dict
-#
-#                    # append this job to the dataset job list
-#                    wl_jobs_json.append(job_entry_data)
-#
-#                kpf_workload_entry = OrderedDict([
-#                                                ('tag', wl.tag),
-#                                                ('jobs', wl_jobs_json)
-#                                                ])
-#
-#                # for each job write the relevant data
-#                kpf_data.append(kpf_workload_entry)
-#
-#            # export kpf file
-#            with open(kpf_filename, 'w') as f:
-#                json.dump(kpf_data, f, indent=4, separators=(',', ': '))
-#        else:
-#            print "filename not specified!"
-#            sys.exit(-1)
-#
-#    @staticmethod
-#    def load_kpf(kpf_filename):
-#        """
-#        Load a kpf file and returns a workload
-#        :param kpf_filename:
-#        :return: jobs
-#        """
-#
-#        # list of job sets to return
-#        workloads = []
-#
-#        with open(kpf_filename, 'r') as f:
-#            workloads_json = json.load(f)
-#
-#        for wl_json in workloads_json:
-#            jobs = wl_json['jobs']
-#            model_jobs = []
-#
-#            for job in jobs:
-#
-#                # create dictionary of timesignals
-#                ts_dict = {}
-#                for tsk, tsv in job['timesignals'].items():
-#                    ts_dict[tsk] = TimeSignal(tsk).from_values(tsk,
-#                                                               tsv['xvalues'],
-#                                                               tsv['yvalues'],
-#                                                               )
-#
-#                model_jobs.append(
-#                                    ModelJob(
-#                                            job_name=job['job_name'],
-#                                            user_name=job['user_name'],
-#                                            cmd_str=job['cmd_str'],
-#                                            queue_name=job['queue_name'],
-#                                            time_queued=job['time_queued'],
-#                                            time_start=job['time_start'],
-#                                            duration=job['duration'],
-#                                            ncpus=job['ncpus'],
-#                                            nnodes=job['nnodes'],
-#                                            stdout=job['stdout'],
-#                                            label=job['label'],
-#                                            timesignals=ts_dict,
-#                                            )
-#                                 )
-#
-#            workloads.append(WorkloadData(jobs=model_jobs, tag=wl_json['tag']))
-#
-#        # return the workload
-#        return workloads
+    print pf == pf2
