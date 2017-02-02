1ff695852afce14fa1b34619f877eb64def18a3c
Antonino Bonanni <antonino.bonanni@ecmwf.int>
2017-02-02 11:29:42 +0000

    fixed few imports

--- kronos_executor/tests/test_jobs.py
+++ kronos_executor/tests/test_jobs.py
@@ -9,7 +9,7 @@ import shutil
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'kronos_py'))
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))), 'bin'))
 
-from job_classes.base_job import BaseJob
+from kronos_executor.job_classes.base_job import BaseJob
 from testutils import scratch_tmpdir
 
 
