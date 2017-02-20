6a6c35ba09b0c39d3a114a3bdca0994ff3a7a60e
Antonino Bonanni <antonino.bonanni@ecmwf.int>
2017-02-20 11:34:15 +0000

    minor clean-up..

--- kronos/core/config/config_format.py
+++ kronos/core/config/config_format.py
@@ -27,15 +27,6 @@ class ConfigFormat(JSONIoFormat):
 
         super(ConfigFormat, self).__init__(created=created, uid=uid)
 
-        # # We either initialise from model jobs, or from processed json data
-        # assert (model_jobs is not None) != (json_jobs is not None)
-        # if model_jobs:
-        #     self.profiled_jobs = [self.parse_model_job(m) for m in model_jobs]
-        # else:
-        #     self.profiled_jobs = json_jobs
-        #
-        # self.workload_tag = workload_tag
-
     @classmethod
     def from_json_data(cls, data):
         """
@@ -52,8 +43,5 @@ class ConfigFormat(JSONIoFormat):
         (which includes headers, etc.)
         """
         output_dict = super(ConfigFormat, self).output_dict()
-        # output_dict.update({
-        #     "profiled_jobs": self.profiled_jobs,
-        #     "workload_tag": self.workload_tag
-        # })
+
         return output_dict
