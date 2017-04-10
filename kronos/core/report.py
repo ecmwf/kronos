2981f273369d33e6e15361401704edc546fc137c
Antonino Bonanni <antonino.bonanni@ecmwf.int>
2017-04-10 15:11:35 +0100

    accuracy measures added into the log

--- kronos/core/report.py
+++ kronos/core/report.py
@@ -5,6 +5,7 @@
 # In applying this licence, ECMWF does not waive the privileges and immunities 
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
+from kronos.core.kronos_tools.print_colour import print_colour
 
 
 class ModelMeasure(object):
@@ -37,13 +38,15 @@ class Report(object):
 
             # simple line if the metrics is a scalar
             if isinstance(m.value, int) or isinstance(m.value, float):
-                print "\n{}: {} (calculated in {})\n".format(m.name, m.value, m.source_function)
+                print_colour("white", "\n{}: {} (calculated in {})\n".format(m.name, m.value, m.source_function), log_level="info")
 
             # print in multi-line is the metric is a dictionary:
             if isinstance(m.value, dict):
-                print "\n{}: (calculated in {})".format(m.name, m.source_function)
+
+                print_colour("white", "\n{}: (calculated in {})".format(m.name, m.source_function))
+
                 for k, v in m.value.iteritems():
-                    print "{}: {}".format(k, v)
+                    print_colour("white", "{}: {}".format(k, v))
 
 
 
