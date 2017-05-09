2f9d71f343d265ef8f11ed339b59bfa76515fa88
Antonino Bonanni <antonino.bonanni@ecmwf.int>
2017-05-09 11:41:49 +0100

    split run_tests and run_tests_exe

--- dev/run_tests_exe.sh
+++ dev/run_tests_exe.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # (C) Copyright 1996-2017 ECMWF.
-# 
+#
 # This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
-# In applying this licence, ECMWF does not waive the privileges and immunities 
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
@@ -18,11 +18,11 @@ export PS1=""
 
 export PATH=${bamboo_working_directory}/miniconda/bin:${PATH}
 
-# tests of the modeller
+# tests of the executor
 
-cd ${bamboo_working_directory}/kronos/core
-source activate test_env
+cd ${bamboo_working_directory}/kronos/executor
+source activate test_env_exe
 for p in `find -maxdepth 3 -mindepth 1 -type d -name tests -not -path "./kronos-synapps/*"`; do
-    PYTHONPATH=`pwd` python ${bamboo_working_directory}/miniconda/envs/test_env/lib/python2.7/site-packages/pytest.py --junitxml="${bamboo_working_directory}/test_output.xml" ${p}
+    PYTHONPATH=`pwd` python ${bamboo_working_directory}/miniconda/envs/test_env_exe/lib/python2.7/site-packages/pytest.py --junitxml="${bamboo_working_directory}/test_output_exe.xml" ${p}
 done
 source deactivate
