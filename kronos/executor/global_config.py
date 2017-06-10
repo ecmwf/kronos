6443da56e847a9e00dbb5632b427e656e51bdd35
Antonino Bonanni <maab@cca-login3.ecmwf.int>
2017-06-10 16:28:34 +0000

    mo the global_config params

--- kronos/executor/global_config.py
+++ kronos/executor/global_config.py
@@ -5,6 +5,6 @@ global_config = {
     "read_file_multiplicity": 16,
     "read_file_size_min": 12,  # Sizes of 2^x in bytes
     "read_file_size_max": 27,
-    "write_file_max_chunk": 268435456,  # 256 Mb
+    "write_file_max_chunk": 4684354560,  # 256 Mb
     "coordinator_binary": None
 }
