63c33b8ecbdedd9268cf478c3fa5bd3aeb792df8
Antonino Bonanni <antonino.bonanni@ecmwf.int>
2018-01-29 10:40:13 +0000

    version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='kronos',
-    version='0.1.3',
+    version='0.1.4',
     description="Workload extraction, modelling and duplication for HPC systems",
     long_description=readme + '\n\n' + history,
     author="Tiago Quintino, Antonino Bonanni, Simon Smart",
