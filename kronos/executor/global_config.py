597cb1bd0c68756e4311547b18a059c4bd2d04cc
Antonino Bonanni <antonino.bonanni@ecmwf.int>
2018-10-01 11:40:15 +0100

    file read multiplicity set to 100 by default

--- kronos/executor/global_config.py
+++ kronos/executor/global_config.py
@@ -2,7 +2,7 @@
 # Global configuration
 
 global_config = {
-    "read_file_multiplicity": 16,
+    "read_file_multiplicity": 100,
     "read_file_size_min": 12,  # Sizes of 2^x in bytes
     "read_file_size_max": 27,
     "write_file_max_chunk": 4684354560,  # 256 Mb
