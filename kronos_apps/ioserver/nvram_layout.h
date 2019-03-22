9770b1ae7cc2f53e7f064b2e0e07c1ab4cef70ea
Antonino Bonanni <antonino.bonanni@ecmwf.int>
2019-03-22 15:01:32 +0000

    minor

--- kronos_apps/ioserver/nvram_layout.h
+++ kronos_apps/ioserver/nvram_layout.h
@@ -4,13 +4,10 @@
 #ifdef HAVE_PMEMIO
 
     #define LAYOUT_NAME "kronos_pobj_layout"
-    /*#define MAX_PMEM_BUF_LEN 1048576*/
     #define MAX_PMEM_BUF_LEN 444
     
     struct kronos_pobj_root {
-
         char buf[MAX_PMEM_BUF_LEN];
-
     };
     
     #endif
