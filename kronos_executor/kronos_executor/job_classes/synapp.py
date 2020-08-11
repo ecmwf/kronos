4043be137d93f65574a64e7002f459caede12787
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2020-08-11 09:45:24 +0100

    Fix missing variable in the synapp job class

--- kronos_executor/kronos_executor/job_classes/synapp.py
+++ kronos_executor/kronos_executor/job_classes/synapp.py
@@ -15,7 +15,7 @@ class SyntheticAppJob(HPCJob):
         :return:
         """
         script_format.update({
-            'procs_per_node': min(self.executor.procs_per_node, nprocs),
+            'procs_per_node': min(self.executor.procs_per_node, script_format['num_procs']),
             'coordinator_binary': self.executor.coordinator_binary,
             'coordinator_library_path': os.path.join( os.path.dirname(self.executor.coordinator_binary),"../lib"),
             'job_name': 'synthApp_{}_{}_{}'.format(script_format['num_procs'], script_format['num_nodes'], self.id)
